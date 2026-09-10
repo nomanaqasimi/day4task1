8c5e8bd5308fa293cb1570ace101d84e2eca258b
nomanaqasimi <nomanaqasimi@gmail.com>
2026-09-10 10:45:36 -0700

    parametrized  function & arrow function

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ function area(l,h){
 }
 RectArea = area(5, 10);
 console.log(RectArea);
+const eligiblity =(age)=>{
+    if(age >= 18){
+        return "Eligible to vote";
+    }else{
+        return "Not eligible to vote";
+    }
+}
+result = eligiblity(20);
+console.log(result);
